dc8edce5e871fcc6b30df7b34ee5ac0d075c3d83
Gabriel dos Santos <neongenesisevangelionchess@gmail.com>
2025-02-04 15:33:24 -0300

    fibonacci excercise

---
+++ fibonacci/index.js
@@ -0,0 +1,13 @@
+var baseValue = 10
+
+function fibonacciGenerator(base) {
+
+    for (i = 0; i < base + 1; i++) {
+
+        console.log('*'.repeat(i))
+
+    }
+
+}
+
+fibonacciGenerator(baseValue)
